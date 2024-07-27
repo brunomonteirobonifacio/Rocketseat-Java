bf6513f9888256bee7f178653d30eaca28e194f6
Bruno <brunombonifacio@gmail.com>
2024-07-27 19:40:43 -0300

    Aula POO - Classes Essenciais

---
+++ Introdução/meu-programa/src/je10_poo/classes_essenciais/ClassesEssenciais.java
@@ -0,0 +1,34 @@
+package je10_poo.classes_essenciais;
+
+
+import java.util.Scanner;
+
+public class ClassesEssenciais {
+    public static void main(String[] args) {
+        String nome = null;
+        Integer idade = null;
+        Double peso = null;
+
+        String linhaArquivo = "Bruno Monteiro;18;58.9";
+
+        Scanner input = new Scanner(linhaArquivo);
+        input.useDelimiter(";");
+
+        int index = 0;
+        while (input.hasNext()) {
+            String info = input.next();
+            switch (index) {
+                case 0 -> nome = info;
+                case 1 -> idade = Integer.parseInt(info);
+                case 2 -> peso = Double.parseDouble(info);
+            }
+
+            index++;
+        }
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+    }
+
+}
