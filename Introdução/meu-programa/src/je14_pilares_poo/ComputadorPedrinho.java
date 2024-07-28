e5d802b0b2e4e19e4153232e1aebe7d90cbfb1c7
Bruno <brunombonifacio@gmail.com>
2024-07-28 11:43:59 -0300

    Aula Pilares POO - Encapsulamento

---
+++ Introdução/meu-programa/src/je14_pilares_poo/ComputadorPedrinho.java
@@ -0,0 +1,10 @@
+package je14_pilares_poo;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNMessenger msnMessenger = new MSNMessenger();
+
+        msnMessenger.enviarMensagem();
+        msnMessenger.receberMensagem();
+    }
+}
