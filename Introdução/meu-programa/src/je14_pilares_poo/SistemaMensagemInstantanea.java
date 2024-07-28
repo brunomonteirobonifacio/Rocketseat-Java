a64b824991d8948a2887a052037abc0f558b0f3b
Bruno <brunombonifacio@gmail.com>
2024-07-28 14:13:02 -0300

    Aula Pilares POO - Herança

---
+++ Introdução/meu-programa/src/je14_pilares_poo/SistemaMensagemInstantanea.java
@@ -0,0 +1,24 @@
+package je14_pilares_poo;
+
+public class SistemaMensagemInstantanea {
+    public void enviarMensagem() {
+        if (validarConectadoInternet()) {
+            System.out.println("Enviando mensagem");
+        } else {
+            System.out.println("Computador offline");
+        }
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    private boolean validarConectadoInternet() {
+        System.out.println("Validando comexão de internet");
+        return true;
+    }
+
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando histórico de mensagens");
+    }
+}
